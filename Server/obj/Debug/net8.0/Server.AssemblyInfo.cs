eede45f489c1159d6f3c43e113d19f49b94efd94
dannynoordamdev <dannynoordam.dev@gmail.com>
2025-05-26 21:16:43 +0200

    fix footer

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c227870d202a175b1ea711f6e829d4340ce9ab93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b990b562aacdecdbfe85a39f256eeed1198a1ee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
